b072d026a50b6d374cb2c81d00967dcf71815fc8
fadlyyy <fadlyrifai95@gmail.com>
2021-08-14 20:36:34 +0700

    push to repo

---
+++ src/SimpleKalkulator.php
@@ -0,0 +1,22 @@
+<?php
+ 
+namespace Sangcahayaid\Kalkulator;
+ 
+/**
+ * Basic Calculator.
+ * 
+ */
+class SimpleKalkulator
+{
+    /**
+     * Menjumlahkan semua data dalam sebuah array.
+     *
+     * @param array $data
+     * @return integer
+     */
+    public static function penjumlahan(array $data)
+    {
+        return array_sum($data);
+    }
+}
+ 
